eefa8bf0cf153f1c4e9f4f2bd8a95ea7687c23eb
RyoTabuki <127320006+RyoTabuki@users.noreply.github.com>
2023-03-09 23:21:10 +0200

    Update KingdomsMoneyScript.py

--- KingdomsMoneyScript.py
+++ KingdomsMoneyScript.py
@@ -1,2 +1,12 @@
-import uniref
-from uniref 
+from uniref import WinUniRef
+
+def SetUnlimitedMoney():
+    ref = WinUniRef("Kingdoms.exe")
+    clazz_mon = ref.find_class_in_image("Assembly-CSharp.dll", "Player_Parameters")
+    offset = clazz_mon.find_field("Gold")
+    addresses = clazz_mon.guess_instance_address()
+    for clx in addresses:
+       offset.set_instance(clx)
+       offset.set_value(999999)
+if __name__ == "__main__":
+    SetUnlimitedMoney()
